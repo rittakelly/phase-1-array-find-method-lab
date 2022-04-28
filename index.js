838e2371bf2b36b391e2bf9311cf001060fc77a4
Ritta <brittanykelly33@gmail.com>
2022-04-27 22:33:02 -0400

    commit tree 90602838a9d1545260d638da6aa11eff223e3eee

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// function superbowlWin(record) {
+//     if (record.result === "W") {
+//         return record.find(record.result);
+//     }
+// }
+
+function superbowlWin(array) {
+    let winningYear = array.find((array) => array.result === "W");
+    if (winningYear) {
+        return winningYear.year;
+    }
+}
+superbowlWin(record);
+
+
+// else if (item.find(year) !== '2015') {
+//     return undefined;
+// }
